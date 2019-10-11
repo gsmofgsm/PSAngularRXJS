1cff8af33b4317e7fddd0d5e1bb4d57249223afe
Qing Ma <qing.ma@isatis-health.nl>
2019-10-11 23:35:58 +0200

    add caching to categories, productsWithCategories, and selectedProduct

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { throwError, combineLatest, EMPTY, BehaviorSubject, Subject, merge } from 'rxjs';
-import { catchError, tap, map, scan } from 'rxjs/operators';
+import { catchError, tap, map, scan, shareReplay } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -38,7 +38,8 @@ export class ProductService {
         category: categories.find(c => product.categoryId === c.id).name,
         searchKey: [product.productName]
       }) as Product)
-    )
+    ),
+    shareReplay(1)
   );
 
   private productSelectedSubject = new BehaviorSubject<number>(0);
@@ -52,7 +53,8 @@ export class ProductService {
       map(([products, selectedProductId]) =>
         products.find(product => product.id === selectedProductId)
       ),
-      tap(product => console.log('selectedProduct: ', product))
+      tap(product => console.log('selectedProduct: ', product)),
+      shareReplay(1)
     );
 
   private productInsertedSubject = new Subject<Product>();
